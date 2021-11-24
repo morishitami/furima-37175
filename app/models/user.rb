65e57838204201e8928cb54927c24110fc3a4f15
morishitami <kshion.with.cad@outlook.jp>
2021-11-24 11:16:00 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :nickname, presence: true
+  validate :lastname, presence: true
+  validate :firstname, presence: true
+  validate :lastname_kana, presence: true
+  validate :firstname_kana, presence: true
+  validate :birthday, presence: true
 end
