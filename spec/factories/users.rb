07ed072e6b5ba8d2255e32709d9219db5229b462
morishitami <kshion.with.cad@outlook.jp>
2021-11-24 23:02:33 +0900

    不要なメモの削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :user do
-    transient do
-      person { Gimei.name}
-    end
 
     nickname {'test'}
     email {Faker::Internet.free_email}
@@ -15,15 +12,3 @@ FactoryBot.define do
     birthday {'1930-12-1'}
   end
 end
-
-#email以外はfakerじゃなくても良かった ↓未検証
-  # nickname {Faker::Name.last_name}
-  # email {Faker::Internet.free_email}
-  # password = Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)
-  # password {password}
-  # password_confirmation {password}
-  # lastname { person.last.kanji}
-  # firstname {person.first.kanji}
-  # lastname_kana {person.last.katakana}
-  # firstname_kana {person.first.katakana}
-  # birthday { Faker::Date.backward }
