32ec606a421608530b2bd93fc758044ded91cc69
morishitami <kshion.with.cad@outlook.jp>
2021-11-24 20:48:58 +0900

    FactoryBot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,17 +4,26 @@ FactoryBot.define do
       person { Gimei.name}
     end
 
-    nickname {Faker::Name.last_name}
+    nickname {'test'}
     email {Faker::Internet.free_email}
-    password {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
-    password {password}
-    password_confiamation {password}
-    lastname { person.last.kanji}
-    firstname {person.first.kanji}
-    lastname_kana {person.last.katakana}
-    firstname_kana {person.first.katakana}
-    birthday { Faker::Date.backward }
+    password {'a12345'}
+    password_confirmation {password}
+    lastname {'てす'}
+    firstname {'と'}
+    lastname_kana {'テス'}
+    firstname_kana {'ト'}
+    birthday {'1930-12-1'}
   end
 end
 
-#email以外はfakerじゃなくても良かったかも
+#email以外はfakerじゃなくても良かった ↓未検証
+  # nickname {Faker::Name.last_name}
+  # email {Faker::Internet.free_email}
+  # password = Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)
+  # password {password}
+  # password_confirmation {password}
+  # lastname { person.last.kanji}
+  # firstname {person.first.kanji}
+  # lastname_kana {person.last.katakana}
+  # firstname_kana {person.first.katakana}
+  # birthday { Faker::Date.backward }
