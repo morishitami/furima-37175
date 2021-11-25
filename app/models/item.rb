b13bddc26fc37b94c8d3f464485cda0be4ecef24
morishitami <kshion.with.cad@outlook.jp>
2021-11-25 12:50:14 +0900

    バリデーションとアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :title, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :postage_id, presence: true
+  validates :state_id, presence: true
+  validates :days_id, presence: true
+  validates :prace, presence: true
+
+  belong_to :user
 end
