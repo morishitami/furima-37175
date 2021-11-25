fdd52665a038580c85854e2cbab5733b90df0b17
morishitami <kshion.with.cad@outlook.jp>
2021-11-25 22:09:13 +0900

    ActiveHashを使ったモデルファイルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,20 @@
 class Item < ApplicationRecord
   validates :title, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :postage_id, presence: true
-  validates :state_id, presence: true
-  validates :days_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "Category can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "Category can't be blank" }
+  validates :postage_id, numericality: { other_than: 1, message: "Category can't be blank" }
+  validates :state_id, numericality: { other_than: 1, message: "Category can't be blank" }
+  validates :day_id, numericality: { other_than: 1, message: "Category can't be blank" }
   validates :prace, presence: true
 
-  belong_to :user
+  belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :state
+  belongs_to :day
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_064810) do
     t.integer "states_id", null: false
     t.integer "postage_id", null: false
     t.integer "state_id", null: false
-    t.integer "days_id", null: false
+    t.integer "day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
