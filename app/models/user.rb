a9f47aff7e93d9babba5d3979ac2474f7b7f273b
morishitami <kshion.with.cad@outlook.jp>
2021-11-25 11:06:30 +0900

    不要な記述の削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: ' Include both letters and numbers'}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: ' Include both letters and numbers'}
   validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' Input full-width characters'}
   validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' Input full-width characters'}
   validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: ' Input full-width katakana characters'}
