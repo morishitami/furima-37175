a756ac37d8cc0104651df5113f281c35c4841acf
morishitami <kshion.with.cad@outlook.jp>
2021-11-26 16:33:27 +0900

    テストコードの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
   validates :title, presence: true
   validates :explanation, presence: true
-  validates :category_id, numericality: { other_than: 1, message: " can't be blank" }
-  validates :status_id, numericality: { other_than: 1, message: " can't be blank" }
-  validates :postage_id, numericality: { other_than: 1, message: " can't be blank" }
-  validates :state_id, numericality: { other_than: 1, message: " can't be blank" }
-  validates :day_id, numericality: { other_than: 1, message: " can't be blank" }
-  validates :price, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, message: " is out of setting range" }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :postage_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :state_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters'}
+  validates :price, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, message: "is out of setting range" }
   validates :image, presence: true
 
   belongs_to :user
