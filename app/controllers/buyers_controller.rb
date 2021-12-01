c3f2ff1c04272b92f964b0d89da3afd3580da729
morishitami <kshion.with.cad@outlook.jp>
2021-12-01 17:01:52 +0900

    card.jsの実装、トークンのサーバでの取込

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,6 +8,12 @@ class BuyersController < ApplicationController
     @item = Item.find(params[:item_id])
     @buyer_address = BuyerAddress.new(buyer_params)
     if @buyer_address.valid?
+      Payjp.api_key = "sk_test_f2cb41eccab84440f4cd364e"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: buyer_params[:token],
+        currency: 'jpy'
+      )
       @buyer_address.save
       redirect_to root_path
     else
@@ -18,6 +24,6 @@ class BuyersController < ApplicationController
   private
 
   def buyer_params
-    params.require(:buyer_address).permit(:postal_code, :state_id, :city, :line1, :line2, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buyer_address).permit(:postal_code, :state_id, :city, :line1, :line2, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,6 +1,6 @@
 class BuyerAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :state_id, :city, :line1, :line2, :phone, :user_id, :item_id
+  attr_accessor :postal_code, :state_id, :city, :line1, :line2, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
