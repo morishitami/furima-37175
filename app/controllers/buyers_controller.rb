5cd371785d803514c40cd10b23ab015308e72baa
morishitami <kshion.with.cad@outlook.jp>
2021-12-01 17:51:20 +0900

    環境変数の設定

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,12 +8,7 @@ class BuyersController < ApplicationController
     @item = Item.find(params[:item_id])
     @buyer_address = BuyerAddress.new(buyer_params)
     if @buyer_address.valid?
-      Payjp.api_key = "sk_test_f2cb41eccab84440f4cd364e"
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: buyer_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @buyer_address.save
       redirect_to root_path
     else
@@ -25,5 +20,14 @@ class BuyersController < ApplicationController
 
   def buyer_params
     params.require(:buyer_address).permit(:postal_code, :state_id, :city, :line1, :line2, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buyer_params[:token],
+      currency: 'jpy'
+    )
   end
 end
